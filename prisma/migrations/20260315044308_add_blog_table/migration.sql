8622797c931d03435ce30b1a4a96f20d019a6b95
rumonkh0 <rummankh0@gmail.com>
2026-03-15 10:50:25 +0600

    blog schema add

---
+++ prisma/migrations/20260315044308_add_blog_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "stats" JSONB,
+    "tags" TEXT[],
+    "mainPhoto" TEXT,
+    "gallery" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
